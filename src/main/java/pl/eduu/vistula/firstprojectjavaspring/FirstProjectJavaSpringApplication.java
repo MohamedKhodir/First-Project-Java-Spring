469c51da84d40732becd19e485a7347dccf65712
Mohamed Khodir <msamirm1@stu.vistula.edu.pl>
2023-12-17 12:01:52 +0100

    Initial commit

---
+++ src/main/java/pl/eduu/vistula/firstprojectjavaspring/FirstProjectJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package pl.eduu.vistula.firstprojectjavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectJavaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstProjectJavaSpringApplication.class, args);
+    }
+
+}
